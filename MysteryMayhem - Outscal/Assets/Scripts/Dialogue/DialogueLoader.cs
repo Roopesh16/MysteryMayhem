21bef4010b52249efa00420d61b535fa8fcbd070
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-28 15:42:30 +0530

    Load dialogues according to member

--- MysteryMayhem - Outscal/Assets/Scripts/Dialogue/DialogueLoader.cs
+++ MysteryMayhem - Outscal/Assets/Scripts/Dialogue/DialogueLoader.cs
@@ -44,12 +44,46 @@ namespace MysteryMayhem.Dialogue
 
         public Queue<string> GetDetDialogueQue(Members member)
         {
-            return null;
+            string detString = null;
+            switch (member)
+            {
+                case Members.JACK:
+                    detString = JsonLoader.LoadJson("DetectiveToJack");
+                    break;
+                case Members.ANNA:
+                    detString = JsonLoader.LoadJson("DetectiveToAnna");
+                    break;
+                case Members.KENNETH:
+                    detString = JsonLoader.LoadJson("DetectiveToKenneth");
+                    break;
+                case Members.BLONTE:
+                    detString = JsonLoader.LoadJson("DetectiveToBlonte");
+                    break;
+            }
+            Queue<string> detectiveQueue = JsonConvert.DeserializeObject<Queue<string>>(detString);
+            return detectiveQueue;
         }
 
         public Queue<string> GetMemberDialogueQue(Members member)
         {
-            return null;
+            string memString = null;
+            switch (member)
+            {
+                case Members.JACK:
+                    memString = JsonLoader.LoadJson("JackToDetective");
+                    break;
+                case Members.ANNA:
+                    memString = JsonLoader.LoadJson("AnnaToDetective");
+                    break;
+                case Members.KENNETH:
+                    memString = JsonLoader.LoadJson("KennethToDetective");
+                    break;
+                case Members.BLONTE:
+                    memString = JsonLoader.LoadJson("BlonteToDetective");
+                    break;
+            }
+            Queue<string> memberQueue = JsonConvert.DeserializeObject<Queue<string>>(memString);
+            return memberQueue;
         }
         #endregion --------------------
 
