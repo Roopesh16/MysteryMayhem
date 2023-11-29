bdb6c5ea5296220b074ef227d8e328c194b49d54
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-29 16:59:42 +0530

    Check distance between object and detective

--- MysteryMayhem - Outscal/Assets/Scripts/Objects/ObjectInfo.cs
+++ MysteryMayhem - Outscal/Assets/Scripts/Objects/ObjectInfo.cs
@@ -1,7 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using MysteryMayhem.Manager;
 
 namespace MysteryMayhem.Objects
 {
@@ -10,7 +11,7 @@ namespace MysteryMayhem.Objects
         BODY,
         COIN,
         RING,
-        POTION
+        NULL
     }
 
     public class ObjectInfo : MonoBehaviour
@@ -21,6 +22,8 @@ namespace MysteryMayhem.Objects
         [SerializeField] private string historyInfo;
         [SerializeField] private ObjectInfoView objectInfoView;
         [SerializeField] private Button infoButton;
+        [SerializeField] private Transform detective;
+        [SerializeField] private float minDistance = 2f;
         #endregion --------------------
 
         #region ---------- Private Variables ----------
@@ -30,9 +33,35 @@ namespace MysteryMayhem.Objects
         #endregion --------------------
 
         #region ---------- Monobehavior Methods ----------
+        private void Awake()
+        {
+            infoButton.onClick.AddListener(InfoButton);
+            infoButton.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (GameManager.Instace.GetGameState() == GameState.PLAY)
+            {
+                if (Vector3.Distance(detective.position, transform.position) <= minDistance)
+                {
+                    infoButton.gameObject.SetActive(true);
+                }
+                else
+                {
+                    infoButton.gameObject.SetActive(false);
+                }
+            }
+        }
         #endregion --------------------
 
         #region ---------- Private Methods ----------
+        private void InfoButton()
+        {
+            infoButton.gameObject.SetActive(false);
+            objectInfoView.DisplayHistoryBox(objectType);
+            GameManager.Instace.SetGameState(GameState.DEDUCTION);
+        }
         #endregion --------------------
 
         #region ---------- Public Methods ----------
