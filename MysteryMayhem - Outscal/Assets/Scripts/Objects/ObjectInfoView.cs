ffb09d1f9bff5007e7bc8d06bc9b4f0b6744dd9c
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-29 16:59:57 +0530

    Display history info

--- MysteryMayhem - Outscal/Assets/Scripts/Objects/ObjectInfoView.cs
+++ MysteryMayhem - Outscal/Assets/Scripts/Objects/ObjectInfoView.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using MysteryMayhem.Manager;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,9 +14,11 @@ namespace MysteryMayhem.Objects
         [SerializeField] private TextMeshProUGUI historyInfoText;
         [SerializeField] private Button closeButton;
         [SerializeField] private TextMeshProUGUI[] namesText = new TextMeshProUGUI[3];
+        [SerializeField] private ObjectInfo[] objects = new ObjectInfo[3];
         #endregion --------------------
 
         #region ---------- Private Variables ----------
+        private ObjectType objectType = ObjectType.NULL;
         #endregion --------------------
 
         #region ---------- Public Variables ----------
@@ -24,23 +28,50 @@ namespace MysteryMayhem.Objects
         private void Awake()
         {
             historyInfoBox.SetActive(false);
+            DisableNameText();
             closeButton.onClick.AddListener(CloseHistoryBox);
         }
         #endregion --------------------
 
         #region ---------- Private Methods ----------
-        private void DisplayHistoryBox(ObjectType objectType)
+
+        private void SetHistoryInfo()
         {
-            historyInfoBox.SetActive(true);
+            historyInfoText.text = objects[(int)objectType].GetHistoryInfo();
+            List<string> namesString = objects[(int)objectType].GetHistoryList();
+            print(namesString.Count);
+            for (int i = 0; i < namesString.Count; i++)
+            {
+                namesText[i].gameObject.SetActive(true);
+                namesText[i].text = namesString[i];
+            }
+        }
+
+        private void DisableNameText()
+        {
+            for (int i = 0; i < namesText.Length; i++)
+            {
+                namesText[i].gameObject.SetActive(false);
+            }
         }
 
         private void CloseHistoryBox()
         {
+            GameManager.Instace.SetGameState(GameState.PLAY);
             historyInfoBox.SetActive(false);
+            historyInfoText.text = "";
+            DisableNameText();
         }
+
         #endregion --------------------
 
         #region ---------- Public Methods ----------
+        public void DisplayHistoryBox(ObjectType objectType)
+        {
+            this.objectType = objectType;
+            historyInfoBox.SetActive(true);
+            SetHistoryInfo();
+        }
         #endregion --------------------
     }
 }
